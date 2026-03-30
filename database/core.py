209664c90ef7ff7043657ce33fd2da4cc7585b62
a1lock <fogfogfogpak@mail.ru>
2026-03-30 22:17:40 +0300

    fix: all bags fixed

--- database/core.py
+++ database/core.py
@@ -1,7 +1,14 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from config import settings
 
-engine = create_async_engine(settings.database_url, echo=False)
+engine = create_async_engine(
+    settings.database_url,
+    echo=False,
+    pool_size=10,         # Сколько держать открытых соединений
+    max_overflow=20,      # Сколько можно создать сверх лимита в пике
+    pool_timeout=30       # Сколько ждать свободного места в пуле
+)
+
 async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 # Dependency для получения сессии (пригодится в будущем)
